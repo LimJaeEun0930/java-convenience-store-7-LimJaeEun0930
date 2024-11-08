db626245c27e78dfacad8161049a315a1765eae7
LimJaeEun0930 <je9513666@gmail.com>
2024-11-09 02:24:14 +0900

    refactor: Combine same product (with promotion or no promotion) into one
    기존에 프로모션의 유무로 같은 상품도 다른 개체취급하였는데, 지금은 프로모션 있건 없건 한 상품은 한 개체내에 존재하도록 함
    사유: 행사에 대한 프로세스 만들 때 한 개체 내에 한 상품이 존재하도록 하는게 더 편할 것 같다.

--- src/main/java/store/Product.java
+++ src/main/java/store/Product.java
@@ -2,32 +2,54 @@ package store;
 
 import static store.Promotion.promotions;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Optional;
+import java.util.LinkedHashMap;
 
 public class Product {
-    public static final HashMap<String, Product> products = new HashMap();
+    public static final HashMap<String, Product> products = new LinkedHashMap<>();
     private String name;
     private int price;
     private int quantity;
+    private int promotionQuantity;
     private Promotion promotion;
 
     public Product(String name, int price, int quantity, Promotion promotion) {
         this.name = name;
         this.price = price;
-        this.quantity = quantity;
         this.promotion = promotion;
+        if (promotion != null) {
+            this.promotionQuantity = quantity;
+        }
+        if (promotion == null) {
+            this.quantity = quantity;
+        }
     }
 
+    public void setAnotherQuantity(int quantity, Promotion promotion) {
+        if (promotion == null) {
+            this.quantity = quantity;
+        }
+        if (promotion != null) {
+            this.promotionQuantity = quantity;
+        }
+    }
     @Override
     public String toString() {
-        StringBuffer sb = new StringBuffer();
-        String promotionStr = "null";
-        if (this.promotion != null) {
-            promotionStr = this.promotion.getPromotionName();
+        if (promotion == null) {
+            return toStringWithNoPromtion();
         }
-        sb.append(name).append(',').append(price).append(',').append(quantity).append(',').append(promotionStr);
+        String toStringWithPromo = toStringWithPromotion() + System.lineSeparator() + toStringWithNoPromtion();
+        return toStringWithPromo;
+    }
+
+    private String toStringWithNoPromtion() {
+        StringBuffer sb = new StringBuffer();
+        sb.append(name).append(',').append(price).append(',').append(quantity);
+        return sb.toString();
+    }
+    private String toStringWithPromotion() {
+        StringBuffer sb = new StringBuffer();
+        sb.append(name).append(',').append(price).append(',').append(promotionQuantity).append(',').append(promotion.getName());
         return sb.toString();
     }
 
@@ -36,7 +58,11 @@ public class Product {
         String name = parts[0];
         int price = Integer.parseInt(parts[1]);
         int quantity = Integer.parseInt(parts[2]);
-        Promotion promozion = promotions.get(name);
+        Promotion promozion = promotions.get(parts[3]);
+        if (products.get(name) != null) {
+            products.get(name).setAnotherQuantity(quantity, promozion);
+            return;
+        }
         products.put(name, new Product(name, price, quantity, promozion));
          }
 
@@ -52,9 +78,13 @@ public class Product {
         return quantity;
     }
 
-    public String getPromotion() {
+    public int getPromotionQuantity() {
+        return promotionQuantity;
+    }
+
+    public String getPromotionName() {
         if (promotion != null) {
-            return promotion.getPromotionName();
+            return promotion.getName();
         }
         return "";
     }
